62bf9551851aada7e568ef1098dd2994866fccc2
rahulkkamble <kamblerk1@gmail.com>
2025-09-19 04:54:19 +0530

    some modifications remained

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/luxury-reimagined/",
+  base: "/luxury-reimagined",
   server: {
     host: "::",
     port: 8080,
